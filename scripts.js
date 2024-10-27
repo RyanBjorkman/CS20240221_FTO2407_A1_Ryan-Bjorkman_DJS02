4aca3e728dc0534172ada6651acdc6d0c5989442
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-10-27 10:19:10 +0200

    prevented division by 0

--- scripts.js
+++ scripts.js
@@ -5,5 +5,14 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  // Check if the divider is 0
+  if (divider === 0) {
+    console.error("Divider can't be 0!");
+    result.innerText = "Divider can't be 0!";
+    return;
+  }
+
   result.innerText = dividend / divider;
 });
+
